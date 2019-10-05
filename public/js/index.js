43eb21bfb999ef32a198e236cd9dfcf1b734a55d
Dalton <dpelkey98@outlook.com>
2019-10-05 16:21:49 -0500

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    function animateCSS(element, animationName, callback) {
+        var node = $(element)[0];
+        node.classList.add('animated', animationName)
+
+        function handleAnimationEnd() {
+            node.classList.remove('animated', animationName)
+            node.removeEventListener('animationend', handleAnimationEnd)
+
+            if (typeof callback === 'function') callback()
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd)
+    }
+
+});
